8ee5cf0ce8eddee0bc68f31eb9e25ddb223f6968
suxyu <23621197+suxyu@users.noreply.github.com>
2023-10-10 19:25:33 +1100

    update

---
+++ script.js
@@ -0,0 +1,8 @@
+// Vega-Lite JSON specification
+const spec = {
+    "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
+    // ... (Your Vega-Lite specification here) ...
+};
+
+// Embed the Vega-Lite visualization in the placeholder div
+vegaEmbed('#vis', spec);
