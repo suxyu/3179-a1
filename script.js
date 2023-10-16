37381edb6240e6be6545fc5275aa20b93aabb5dd
suxyu <23621197+suxyu@users.noreply.github.com>
2023-10-16 16:10:16 +1100

    update

--- script.js
+++
@@ -1,5 +0,0 @@
-// URL of the Vega-Lite JSON specification
-const specURL = "https://raw.githubusercontent.com/suxyu/3179-a2/main/wing_aspect_ratio_heat.json";
-
-// Embed the Vega-Lite visualization in the placeholder div
-vegaEmbed('#vis', specURL);
